bae6df97d28c67500f81506b69f8d7c1278f48c4
JosephShin <joespam9999@gmail.com>
2022-05-22 20:11:37 -0400

    commit tree 57d023cc6b892a2a37989c683f0ea3939ab660d7

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require_relative './user'
 
+class Student < User
+    # binding.pry
+    def initialize(knowledge = [])
+        @knowledge = knowledge
+    end
+
+    def learn string
+        @knowledge << string
+    end
+
+    def knowledge
+        @knowledge
+    end   
 end
